17b946c5b99f5226bcdf1df7247c36b89bd8bc63
tomcarter <tomcarter017@gmail.com>
2025-03-04 21:24:17 +0000

    add about and pic

--- portfolio/eleventy.config.js
+++ portfolio/eleventy.config.js
@@ -1,5 +1,5 @@
 export default function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("portfolio/css/");
     eleventyConfig.addPassthroughCopy("portfolio/js/");
-    eleventyConfig.addPassthroughCopy("portfolio/noun-graph-network-6418833.svg")
+    eleventyConfig.addPassthroughCopy("portfolio/assets/")
   };
